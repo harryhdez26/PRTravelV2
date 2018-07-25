57d1b01c3fe449c9e85d0d1b03cdfc08b6162945
Harry Hernandez <hh499@cornell.com>
2018-07-25 00:22:06 -0400

    Reading attractions from ts.

--- src/pages/attractions/attractions.ts
+++ src/pages/attractions/attractions.ts
@@ -7,11 +7,36 @@ import { ModalContentPage } from '../attractionsModal/attractionsModal';
   templateUrl: 'attractions.html'
 })
 export class AttractionsPage {
+  attractions;
 
   constructor(
   	public navCtrl: NavController,
   	public modalCtrl: ModalController
-  ) {}
+  ) {
+    this.attractions = [
+      {
+        name:"Flamenco Beach",
+        town:"Culebra",
+        img:"assets/imgs/flamenco.jpg"
+      }, {
+        name:"Las Cascadas Water Park",
+        town:"Aguadilla",
+        img:"assets/imgs/cascadas.jpg"
+      }, {
+        name:"Toro Verde",
+        town:"Orocovis",
+        img:"assets/imgs/toroverde.jpg"
+      }, {
+        name:"El Yunque National Forest",
+        town:"Rio Grande",
+        img:"assets/imgs/yunque.jpg"
+      }, {
+        name:"Caja de Muertos Island",
+        town:"Ponce",
+        img:"assets/imgs/cajademuertos.jpg"
+      }
+    ];
+  }
 
   openModal(characterNum) {
 
